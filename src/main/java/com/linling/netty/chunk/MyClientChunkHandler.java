e101726b6f7d01df3564433d3a7b160c7a815d28
bayern_linling1028 <470888961@qq.com>
2017-12-28 17:48:59 +0800

    回退

--- src/main/java/com/linling/netty/chunk/MyClientChunkHandler.java
+++ src/main/java/com/linling/netty/chunk/MyClientChunkHandler.java
@@ -16,9 +16,6 @@ public class MyClientChunkHandler extends ChannelOutboundHandlerAdapter {
     public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
         if(msg instanceof ByteBuf) {
             ByteBuf buf = (ByteBuf)msg;
-            /*
-            // 使用Netty提供的ByteBufInputStream 来提高性能，底层不会再去拷贝数据。
-            // 而ByteInputStream则会将ByteBuf中的数据拷贝到ByteInputStream中。并且这里此处的ByteBuf如果是堆外的内存数据的话，就更不好了，数据还需要从堆外拷贝到堆内。
             ByteInputStream in = new ByteInputStream();
             byte[] data = null;
             if(buf.hasArray()) {
@@ -32,10 +29,6 @@ public class MyClientChunkHandler extends ChannelOutboundHandlerAdapter {
             }
             System.out.println("===== data length : " + data.length);
             in.setBuf(data);
-            */
-
-
-            ByteBufInputStream in = new ByteBufInputStream(buf);
             ChunkedStream stream = new ChunkedStream(in);
 
             ChannelProgressivePromise progressivePromise =  ctx.channel().newProgressivePromise();
@@ -56,8 +49,7 @@ public class MyClientChunkHandler extends ChannelOutboundHandlerAdapter {
                 }
             });
 
-            // 因为 ByteBufInputStream 底层引用的还是当前write操作传进来的msg（即，ByteBuf）对象。因此在使用ByteBufInputStream实现时，这里是不能调用资源释放。但在使用原先的ByteInputStream方式实现时，一定要记得调用资源释放，否则可能出现内存泄漏。
-//            ReferenceCountUtil.release(msg);
+            ReferenceCountUtil.release(msg);
             ctx.write(stream, progressivePromise);
         } else {
             super.write(ctx, msg, promise);
---
+++ src/main/java/com/linling/netty/trafficshaping/oom/MyGlobalTrafficShapingHandler.java
@@ -0,0 +1,7 @@
+package com.linling.netty.trafficshaping.oom;
+
+/**
+ * Created by hayashishaochie on 2017/12/28.
+ */
+public class MyGlobalTrafficShapingHandler {
+}
